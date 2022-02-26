6e6ec5e2f83b2276d10152277530cb45171ade5a
dew_drop355 <ernin32@gmail.com>
2022-02-26 17:26:31 -0500

    made new branch

--- js/phaserGame.js
+++ js/phaserGame.js
@@ -20,7 +20,7 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    // this.load.setBaseURL('http://labs.phaser.io');
+    //this.load.setBaseURL('../');
 
     // this.load.image('sky', 'assets/skies/space3.png');
     // this.load.image('logo', 'assets/sprites/phaser3-logo.png');
