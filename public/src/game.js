20a0c56e9e733d6f12d29a56b021d8dfe59472b8
Mildness Onyekwere <mno5865@rit.edu>
2022-02-27 05:35:14 -0500

    adding anger mechanics to main game

--- public/src/game.js
+++ public/src/game.js
@@ -1,29 +1,3 @@
-/**
- *  @author Mildness
- *  @description : game implementation
- * 
- */
-
-/*import "../html/infoScreen.html"
-
-function setup(){
-    var canvas = document.getElementById("MyCanvas");
-    var ctx = canvas.getContext("2D");
-
-}
-
-function moveTile(){
-
-}
-
-function resetGame(){
-}
-
-
-function begin(){
-    document.getElementById("myGame").style.display = "block";
-}*/
-
 function allowDrop(ev) {
     ev.preventDefault();
   }
