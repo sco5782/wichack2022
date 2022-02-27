f5d14e733eda29aba6083e751fcdd2728ffac8f7
dew_drop355 <ernin32@gmail.com>
2022-02-26 21:49:35 -0500

    opening message and some tiles

--- public/src/phaserGame.js
+++ public/src/phaserGame.js
@@ -20,30 +20,37 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('bgtile', '../assets/blank_tile.png')
+    this.load.image('bgtile', '../assets/blank_tile.png');
     this.load.image('tile', '../assets/tile.png');
-    
+    this.load.image('text_box', '../assets/text_box.png');
+    this.load.image('button', '../assets/boxForText.png');
 }
 
 function create ()
 {
     bg_tiles = this.add.group({key: 'bgtile', repeat: 63, setScale: {x: 2, y:2 }});
+    bad_tiles = this.add.group({key: 'tile', repeat: 2, setScale: {x:2, y:2}});
     var grid_x = (this.game.config.width / 2) - 200;
-    Phaser.Actions.GridAlign(bg_tiles.getChildren(), { width: 8, cellWidth: 64, cellHeight: 64, x: grid_x, y: 100 });
+    var tile_size = 64;
 
-    // var particles = this.add.particles('red');
+    bad_tiles.getChildren().forEach(function(tile){
+        tile.x = Math.round(Math.random() * 7) * tile_size + grid_x - 10;
+        tile.y = Math.round(Math.random() * 7) * tile_size + 100 - 10;
 
-    // var emitter = particles.createEmitter({
-    //     speed: 100,
-    //     scale: { start: 1, end: 0 },
-    //     blendMode: 'ADD'
-    // });
+    }, this);
 
-    // var logo = this.physics.add.image(400, 100, 'logo');
+    Phaser.Actions.GridAlign(bg_tiles.getChildren(), { width: 8, cellWidth: tile_size, cellHeight: tile_size, x: grid_x, y: 100 });
 
-    // logo.setVelocity(100, 200);
-    // logo.setBounce(1, 1);
-    // logo.setCollideWorldBounds(true);
+    textBox = this.add.image(350, 450, 'text_box').setScale(5);
+    welcome_msg = this.add.text(150, 430, "Today has been an alright day. I'm fine.");
+    ok_button = this.add.image(500, 470, 'button').setScale(.15).setInteractive();
+    ok_text = this.add.text(480, 460, "Okay").setColor("black");
 
-    // emitter.startFollow(logo);
+    ok_button.once('pointerdown', function(){
+        ok_button.setActive(false).setVisible(false);
+        welcome_msg.setActive(false).setVisible(false);
+        textBox.setActive(false).setVisible(false);
+        ok_text.setActive(false).setVisible(false);
+    });
+
 }
