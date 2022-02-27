7ba5b1bafefad0622cdcad4aecb80c872b20a5e0
dew_drop355 <ernin32@gmail.com>
2022-02-27 09:28:37 -0500

    larger assets, centered game

--- public/src/phaserGame.js
+++ public/src/phaserGame.js
@@ -28,21 +28,21 @@ function preload ()
 
 function create ()
 {
-    bg_tiles = this.add.group({key: 'bgtile', repeat: 63, setScale: {x: 2, y:2 }});
-    bad_tiles = this.add.group({key: 'tile', repeat: 2, setScale: {x:2, y:2}});
-    good_tile = this.add.image(0, 0, 'tile').setScale(2);
+    bg_tiles = this.add.group({key: 'bgtile', repeat: 48, setScale: {x: 0.15, y: 0.15 }});
+    bad_tiles = this.add.group({key: 'tile', repeat: 2, setScale: {x:0.15, y:0.15 }});
+    good_tile = this.add.image(0, 0, 'tile').setScale(0.15);
     bad_tiles.add(good_tile);
 
-    var grid_x = (this.game.config.width / 2) - 200;
-    var tile_size = 64;
+    var grid_x = (this.game.config.width / 2) - 460;
+    var tile_size = 75;
 
     bad_tiles.getChildren().forEach(function(tile){
-        tile.x = Math.round(Math.random() * 7) * tile_size + grid_x - 15;
-        tile.y = Math.round(Math.random() * 7) * tile_size + 100 - 10;
+        tile.x = ((Math.round(Math.random() * 6) + 1) * tile_size + 3);
+        tile.y = ((Math.round(Math.random() * 6) + 1) * tile_size - 12);
         tile.setInteractive();
     }, this);
 
-    Phaser.Actions.GridAlign(bg_tiles.getChildren(), { width: 8, cellWidth: tile_size, cellHeight: tile_size, x: grid_x, y: 100 });
+    Phaser.Actions.GridAlign(bg_tiles.getChildren(), { width: 7, cellWidth: tile_size, cellHeight: tile_size, x: grid_x, y: -150 });
 
     textBox = this.add.image(350, 450, 'text_box').setScale(5);
     welcome_msg = this.add.text(150, 430, "Today has been an alright day. I'm fine.");
@@ -65,7 +65,7 @@ function create ()
             d_text.y = game.input.mousePointer.y;
             d_text.setActive(true).setVisible(true);
             d_text.alpha = 0;
-            d_text.setText(discouraging[Math.round(Math.random() * 4)]);
+            d_text.setText(discouraging[Math.round(Math.random() * 3)]);
             this.tweens.add({
                 targets: d_text,
                 alpha: 1,  
