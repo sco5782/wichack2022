8fa4151b3dabb44dc19e6a33d0bb6e65f2dee9a1
dew_drop355 <ernin32@gmail.com>
2022-02-26 23:35:23 -0500

    Finished basic denial functionality, working on adding transition to next level

--- public/src/phaserGame.js
+++ public/src/phaserGame.js
@@ -30,13 +30,16 @@ function create ()
 {
     bg_tiles = this.add.group({key: 'bgtile', repeat: 63, setScale: {x: 2, y:2 }});
     bad_tiles = this.add.group({key: 'tile', repeat: 2, setScale: {x:2, y:2}});
+    good_tile = this.add.image(0, 0, 'tile').setScale(2);
+    bad_tiles.add(good_tile);
+
     var grid_x = (this.game.config.width / 2) - 200;
     var tile_size = 64;
 
     bad_tiles.getChildren().forEach(function(tile){
-        tile.x = Math.round(Math.random() * 7) * tile_size + grid_x - 10;
+        tile.x = Math.round(Math.random() * 7) * tile_size + grid_x - 15;
         tile.y = Math.round(Math.random() * 7) * tile_size + 100 - 10;
-
+        tile.setInteractive();
     }, this);
 
     Phaser.Actions.GridAlign(bg_tiles.getChildren(), { width: 8, cellWidth: tile_size, cellHeight: tile_size, x: grid_x, y: 100 });
@@ -45,6 +48,8 @@ function create ()
     welcome_msg = this.add.text(150, 430, "Today has been an alright day. I'm fine.");
     ok_button = this.add.image(500, 470, 'button').setScale(.15).setInteractive();
     ok_text = this.add.text(480, 460, "Okay").setColor("black");
+    d_text = this.add.text(0, 0, "Default text").setActive(false).setVisible(false)
+    d_text.setShadow(1, 2, "#000000", 2, true, true);
 
     ok_button.once('pointerdown', function(){
         ok_button.setActive(false).setVisible(false);
@@ -53,4 +58,43 @@ function create ()
         ok_text.setActive(false).setVisible(false);
     });
 
+    discouraging = ["I don't want to think about it.", "No.", "Let's do something else...", "Ugh."];
+    bad_tiles.getChildren().forEach(function(tile){
+        tile.on("pointerover", function(){
+            d_text.x = game.input.mousePointer.x;
+            d_text.y = game.input.mousePointer.y;
+            d_text.setActive(true).setVisible(true);
+            d_text.alpha = 0;
+            d_text.setText(discouraging[Math.round(Math.random() * 4)]);
+            this.tweens.add({
+                targets: d_text,
+                alpha: 1,  
+                duration: 500,
+                ease: 'Sine.easeOut'
+            });
+        }, this);
+        tile.on("pointerout", function(){
+            this.tweens.add({
+                targets: d_text,
+                alpha: 0,  
+                duration: 500,
+                ease: 'Sine.easeOut'
+            });
+            d_text.alpha = 0;
+            d_text.setVisible(false);
+        }, this);
+    }, this);
+
+    good_tile.on("pointerdown", function(){
+        //var url = ;
+        var s = window.open(url);
+        if (s && s.focus)
+        {
+            s.focus();
+        }
+        else if (!s)
+        {
+            window.location.href = url;
+        }
+    })
 }
